91c0d437771dc894cd7f30560b514904c5f18570
Jiří Beneš <mail@jiribenes.com>
2021-06-13 23:15:04 +0200

    Change nix file as stdenv.lib has been deprecated

--- servant-prometheus.nix
+++ servant-prometheus.nix
@@ -1,7 +1,24 @@
-{ mkDerivation, aeson, base, bytestring, hspec, http-client
-, http-types, process, prometheus-client, servant, servant-auth
-, servant-client, servant-multipart, servant-server, stdenv, text
-, time, transformers, unordered-containers, wai, warp
+{ mkDerivation
+, aeson
+, base
+, bytestring
+, hspec
+, http-client
+, http-types
+, process
+, prometheus-client
+, servant
+, servant-auth
+, servant-client
+, servant-multipart
+, servant-server
+, lib
+, text
+, time
+, transformers
+, unordered-containers
+, wai
+, warp
 }:
 mkDerivation {
   pname = "servant-prometheus";
@@ -10,17 +27,44 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base bytestring http-types prometheus-client servant servant-auth
-    servant-multipart servant-server text time unordered-containers wai
+    base
+    bytestring
+    http-types
+    prometheus-client
+    servant
+    servant-auth
+    servant-multipart
+    servant-server
+    text
+    time
+    unordered-containers
+    wai
   ];
   executableHaskellDepends = [
-    aeson base process prometheus-client servant-server text wai warp
+    aeson
+    base
+    process
+    prometheus-client
+    servant-server
+    text
+    wai
+    warp
   ];
   testHaskellDepends = [
-    aeson base hspec http-client prometheus-client servant
-    servant-client servant-server text transformers
-    unordered-containers wai warp
+    aeson
+    base
+    hspec
+    http-client
+    prometheus-client
+    servant
+    servant-client
+    servant-server
+    text
+    transformers
+    unordered-containers
+    wai
+    warp
   ];
   description = "Helpers for using prometheus with servant";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
